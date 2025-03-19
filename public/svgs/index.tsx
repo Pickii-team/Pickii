52da3e2e4fc9d37d3bd343c84b5a616df0b02066
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-03-20 01:57:14 +0900

    Design: svg 추가

---
+++ public/svgs/index.tsx
@@ -0,0 +1,17 @@
+import Logo from './logo.svg';
+import IconClose from './close.svg';
+import IconSearch from './search.svg';
+import IconFire from './fire.svg';
+import IconSmileEmoji from './smile-emoji.svg';
+import IconUser from './user.svg';
+import IconVs from './vs.svg';
+
+export {
+  Logo,
+  IconClose,
+  IconSearch,
+  IconFire,
+  IconSmileEmoji,
+  IconUser,
+  IconVs,
+};
