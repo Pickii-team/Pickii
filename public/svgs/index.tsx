18860fb507497d8f038a04eef21881637ab7df5b
Da-Youn <dayoun202@gmail.com>
2025-03-21 17:09:47 +0900

    Fix: 병합 충돌 수정

--- public/svgs/index.tsx
+++ public/svgs/index.tsx
@@ -5,16 +5,13 @@ import IconFire from './fire.svg';
 import IconSmileEmoji from './smile-emoji.svg';
 import IconUser from './user.svg';
 import IconVs from './vs.svg';
-<<<<<<< HEAD
 import IconSend from './send.svg';
 import IconLeftArrow from './left-arrow.svg';
-=======
 import IconElipsis from './elipsis.svg';
 import IconFolderCheck from './folder-check.svg';
 import IconDangerCircle from './danger-circle.svg';
 import IconKakao from './kakao.svg';
 import IconGoogle from './google.svg';
->>>>>>> b0b4fff02b2a2b427cc881c4f840552cbede79bb
 
 export {
   Logo,
@@ -24,14 +21,11 @@ export {
   IconSmileEmoji,
   IconUser,
   IconVs,
-<<<<<<< HEAD
   IconSend,
   IconLeftArrow,
-=======
   IconElipsis,
   IconFolderCheck,
   IconDangerCircle,
   IconKakao,
   IconGoogle,
->>>>>>> b0b4fff02b2a2b427cc881c4f840552cbede79bb
 };
