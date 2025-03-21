9f0d3f3b847c3e460193d18281fba7f0f009f4a6
Da-Youn <dayoun202@gmail.com>
2025-03-21 16:46:10 +0900

    Refactor: 질문 상세 페이지 컨테이너 폴더로 분리 #5

---
+++ src/container/question/detail/comments-section/index.tsx
@@ -0,0 +1,30 @@
+// src/container/question/detail/comments-section.tsx
+import CommentItem from './comment-item';
+
+interface CommentsSectionProps {
+  comments: {
+    username: string;
+    commentText: string;
+    avatarUrl: string;
+  }[];
+}
+
+const CommentsSection = ({ comments }: CommentsSectionProps) => {
+  return (
+    <section>
+      <h2 className='border-b border-gray-4 pb-8 pl-16 text-small-01'>
+        댓글({comments.length})
+      </h2>
+      {comments.map((comment, index) => (
+        <CommentItem
+          key={index}
+          username={comment.username}
+          commentText={comment.commentText}
+          avatarUrl={comment.avatarUrl}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default CommentsSection;
---
+++ src/container/question/detail/vote-result-section/vote-result-carousel.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Carousel from '@/src/components/Carousel';
+import Image from 'next/image';
+
+interface Card {
+  id: number;
+  [key: string]: any;
+}
+
+interface VoteResultCarouselProps {
+  cards: Card[];
+}
+
+export default function VoteResultCarousel({ cards }: VoteResultCarouselProps) {
+  return (
+    <Carousel cards={cards}>
+      {(card) => (
+        <div>
+          <Image
+            src={card.imageUrl}
+            alt=''
+            fill
+            className='overflow-hidden rounded-12'
+            draggable={false}
+          />
+          <div className='absolute bottom-0 flex h-74 w-full items-end overflow-hidden rounded-b-8 bg-image-overlay px-12 py-8'>
+            <p className='text-small-01 text-white'>
+              <span className='text-title-04'>{card.answer}</span>
+              이(가)
+              <br />
+              {card.text}
+            </p>
+          </div>
+        </div>
+      )}
+    </Carousel>
+  );
+}
