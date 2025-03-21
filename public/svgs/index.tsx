e9f8a5628c0147340e6cfe09dce2d7c609644e6a
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-03-21 16:46:00 +0900

    Design: close svg 추가

--- public/svgs/index.tsx
+++ public/svgs/index.tsx
@@ -1,17 +1,19 @@
 import Logo from './logo.svg';
-import IconClose from './close.svg';
+import IconCloseCircle from './close-circle.svg';
 import IconSearch from './search.svg';
 import IconFire from './fire.svg';
 import IconSmileEmoji from './smile-emoji.svg';
 import IconUser from './user.svg';
 import IconVs from './vs.svg';
+import IconClose from './close.svg';
 
 export {
   Logo,
-  IconClose,
+  IconCloseCircle,
   IconSearch,
   IconFire,
   IconSmileEmoji,
   IconUser,
   IconVs,
+  IconClose,
 };
