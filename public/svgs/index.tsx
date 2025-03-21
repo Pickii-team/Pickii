6d4f5200789f9b8e3a2b87e236075a4e0f742e4a
Da-Youn <dayoun202@gmail.com>
2025-03-21 16:42:16 +0900

    Chore: svg 추가 #5

--- public/svgs/index.tsx
+++ public/svgs/index.tsx
@@ -5,6 +5,8 @@ import IconFire from './fire.svg';
 import IconSmileEmoji from './smile-emoji.svg';
 import IconUser from './user.svg';
 import IconVs from './vs.svg';
+import IconSend from './send.svg';
+import IconLeftArrow from './left-arrow.svg';
 
 export {
   Logo,
@@ -14,4 +16,6 @@ export {
   IconSmileEmoji,
   IconUser,
   IconVs,
+  IconSend,
+  IconLeftArrow,
 };
