77f9a0a8cd161e9394db28596cad451c02aa4067
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-03-21 16:45:20 +0900

    Chore: 목업 데이터 생성

---
+++ src/lib/mockData.ts
@@ -0,0 +1,30 @@
+export const QUESTIONS_STAT = [
+  { id: 1, questionId: 1, totalVotesA: 72, totalVotesB: 28 },
+  { id: 2, questionId: 2, totalVotesA: 35, totalVotesB: 65 },
+];
+export const QUESTIONS = [
+  {
+    id: 1,
+    question_text: '복숭아',
+    answer_a: '물복',
+    answer_b: '딱복',
+    answer_a_desc: '물렁물렁한 복숭아',
+    answer_b_desc: '아삭아삭한 복숭아',
+    answer_a_img:
+      'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
+    answer_b_img:
+      'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
+  },
+  {
+    id: 2,
+    question_text: '복숭아2',
+    answer_a: '물복',
+    answer_b: '딱복',
+    answer_a_desc: '물렁물렁한 복숭아',
+    answer_b_desc: '아삭아삭한 복숭아',
+    answer_a_img:
+      'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
+    answer_b_img:
+      'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
+  },
+];
