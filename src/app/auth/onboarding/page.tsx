be494f480c711b0c6153ad0d914e56617689e2ff
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-03-31 21:23:13 +0900

    Refactor: 버튼 컴포넌트 분리

--- src/app/auth/onboarding/page.tsx
+++ src/app/auth/onboarding/page.tsx
@@ -1,17 +1,12 @@
-'use client';
-
-import Button from '@/src/components/Button';
 import NicknameSection from '@/src/containers/auth/onboarding/nickname-section';
 import { Logo } from '@/public/svgs';
 
-export default function () {
+export default function OnBoarding() {
   return (
     <>
       <Logo height='48' className='mx-auto mb-80' />
+
       <NicknameSection />
-      <Button variant='filled' buttonColor='primary-variant'>
-        가입 완료하기
-      </Button>
     </>
   );
 }
