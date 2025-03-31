4c5a66f0d5d85dd10cf29f167bae4093cb4013b5
easyxxu <107910342+easyxxu@users.noreply.github.com>
2025-03-31 21:22:27 +0900

    Refactor: 함수명 변경

--- src/actions/auth/create-oauth-login.ts
+++ src/actions/auth/create-oauth-login.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/src/utils/supabase/server';
 import { Provider } from '@supabase/supabase-js';
 import { redirect } from 'next/navigation';
 
-export async function createOauthLogin(provider: Provider) {
+export async function CreateOauthLogin(provider: Provider) {
   const supabase = await createClient();
   const auth_callback_url = `${process.env.SITE_URL}/auth/callback`;
   const { data, error } = await supabase.auth.signInWithOAuth({
--- src/containers/auth/login/login-button-section/index.tsx
+++ src/containers/auth/login/login-button-section/index.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { IconKakao, IconGoogle } from '@/public/svgs';
-import { createOauthLogin } from '@/src/actions/auth/create-oauth-login';
+import { CreateOauthLogin } from '@/src/actions/auth/create-oauth-login';
 import Button from '@/src/components/Button';
 type Props = {};
 
@@ -13,7 +13,7 @@ export default function LoginButtonSection({}: Props) {
         variant='custom'
         icon={<IconKakao className='shrink-0' />}
         className='flex items-center bg-[#FEE500] text-black'
-        onClick={() => createOauthLogin('kakao')}
+        onClick={() => CreateOauthLogin('kakao')}
       >
         카카오 로그인
       </Button>
@@ -21,7 +21,7 @@ export default function LoginButtonSection({}: Props) {
         variant='custom'
         icon={<IconGoogle className='shrink-0' />}
         className='border-gray-08 flex items-center border bg-white text-gray-8'
-        onClick={() => createOauthLogin('google')}
+        onClick={() => CreateOauthLogin('google')}
       >
         구글 로그인
       </Button>
